22c2b2cc54f4c7e4da797364848f6ed026468a5f
lysanderP <lysanderp41@gmail.com>
2017-03-09 15:07:06 -0500

    Added a button test to the gui

--- ResidenceHallManagerGUI.java
+++ ResidenceHallManagerGUI.java
@@ -1,5 +1,34 @@
 /**
  * Created by Lysander on 3/8/17.
  */
-public class ResidenceHallManagerGUI {
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+
+
+public class ResidenceHallManagerGUI extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Test");
+        Button btn = new Button();
+        btn.setText("This is a test");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
