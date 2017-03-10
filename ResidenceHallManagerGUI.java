c6f3c93bc04623e9d5de363dc19818ec158dfd99
lysanderP <lysanderp41@gmail.com>
2017-03-09 23:43:08 -0500

    added all the menu items to a VBox and added that vbox to the menu

--- ResidenceHallManagerGUI.java
+++ ResidenceHallManagerGUI.java
@@ -3,8 +3,7 @@
  */
 import javafx.application.Application;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -15,8 +14,9 @@ public class ResidenceHallManagerGUI extends Application {
     public static Floor[] floors = new Floor[3];
     //menu scene objects
     Button btnA, btnR, btnS, btnM, btnF, btnC, btnP, btnW, btnQ;
-    FlowPane pane1, pane2;
-    Scene menu, scene2;
+    BorderPane menu, pane2;
+    VBox menuItems;
+    Scene scene1, scene2;
     Stage stage;
 
     public static void main(String[] args) {
@@ -24,6 +24,25 @@ public class ResidenceHallManagerGUI extends Application {
     }
     @Override
     public void start(Stage primaryStage) {
-
+        stage = primaryStage;
+        btnA = new Button("Add a student");
+        btnR = new Button("Remove a student");
+        btnS = new Button("Swap students");
+        btnM = new Button("Move a student");
+        btnF = new Button("Select floor");
+        btnC = new Button("Copy floor");
+        btnP = new Button("Display floor");
+        btnW = new Button("Writeup student");
+        btnQ = new Button("Quit");
+        menu = new BorderPane();
+        menuItems = new VBox();
+        menuItems.getChildren().addAll(btnA, btnR, btnS, btnM, btnF, btnC, btnP, btnW, btnQ);
+        menuItems.setSpacing(4);
+        menu.setCenter(menuItems);
+        scene1 = new Scene(menu);
+        stage.setScene(scene1);
+        btnQ.addEventHandler();
+        
+        stage.show();
     }
 }
